a7bd0039f22005ba91b63d1604e41868efd3a583
griush <gerardriush@gmail.com>
2022-01-11 20:43:50 +0100

    Implemented text color change acording to rpms

--- minimal_gear_display.py
+++ minimal_gear_display.py
@@ -1,11 +1,26 @@
-import sys
 import ac
 import acsys
 
+import os
+import sys
+import traceback
+
+try:
+
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'dll'))
+    from third_party.sim_info import info
+
+except Exception:
+        ac.console("LRev : import Error: %s" % traceback.format_exc())
+        ac.log("LRev : import Error: %s" % traceback.format_exc())
+
 l_gear = 0
 l_shadow = 0
+
 gear = 0
 gear_text = "N"
+
+rpms = 0
 
 app_window = 0
 
@@ -37,17 +52,35 @@ def acMain(ac_version):
     ac.setPosition(l_gear, 25, 0)
     ac.setFontAlignment(l_gear, "center")
     ac.setFontSize(l_gear, 85)
+    ac.addRenderCallback(l_gear, acUpdate)
     
     return app_name
 
 def acUpdate(deltaT):
-    global l_gear, gear
+    global l_gear, gear, rpms
 
-    # Window update
+    gear_c = ac.getCarState(0, acsys.CS.Gear)
+
+    """ Window update """
     ac.setBackgroundOpacity(app_window, 0)
 
-    # Gear update
-    gear_c = ac.getCarState(0, acsys.CS.Gear)
+    """ Label color update """
+    maxRPM = info.static.maxRpm
+    rpms = ac.getCarState(0, acsys.CS.RPM)
+    if gear_c > 1:
+        if maxRPM - rpms < 450:
+            ac.setFontColor(l_gear, 1, 0, 0, 1)
+        elif maxRPM - rpms < 1250:
+            ac.setFontColor(l_gear, 0, 1, 0, 1)
+        else:
+            ac.setFontColor(l_gear, 1, 1, 1, 1)
+    else:
+        if maxRPM - rpms < 400:
+            ac.setFontColor(l_gear, 1, 0, 0, 1)
+        else:
+            ac.setFontColor(l_gear, 1, 1, 1, 1)
+    
+    """ Gear text update """
 
     if gear_c != gear:
         gear = gear_c
